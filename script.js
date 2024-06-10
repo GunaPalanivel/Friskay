a46d2b722639a0fdc8ec31cef9a11b979675dd69
GunaPalanivel <gp5901@srmist.edu.in>
2024-06-10 23:51:20 +0530

    style: update font family and navigation styles

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // WIP : Developer will look into this file and write the code to make the application work as expected.
+const menu = document.querySelector("nav ul");
+const menuBtn = document.querySelector(".menu-open");
+const closeBtn = document.querySelector(".menu-close");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
