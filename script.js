f6ad87576a260e9bb5671f84b1a146728bf34a52
gunapalanivel2003 <gunapalanivel2003@gmail.com>
2024-06-11 12:06:37 +0000

    from IDX - Friskay - fixed src issues

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// WIP : Developer will look into this file and write the code to make the application work as expected.
-const menu = document.querySelector("nav ul");
-const menuBtn = document.querySelector(".menu-open");
-const closeBtn = document.querySelector(".menu-close");
+const menu = document.querySelector('nav ul');
+const menuBtn = document.querySelector('.menu-open');
+const closeBtn = document.querySelector('.menu-close');
 
-menuBtn.addEventListener("click", () => {
-  menu.classList.add("open");
-});
+menuBtn.addEventListener('click', () => {
+    menu.classList.add('open')
+})
 
-closeBtn.addEventListener("click", () => {
-  menu.classList.remove("open");
-});
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('open')
+})
